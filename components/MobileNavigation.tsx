395ba5ea42775a2720cda0a410514b7bbe2bf0a0
AungChanNyeinC-137 <aungchannyein29699@gmail.com>
2025-07-05 12:29:42 +0630

    mobile navigation added with Prompts
    mobile navigation added with Prompts, will add the separator later မီးပျက်သွား 😠

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+'use client'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { usePathname } from 'next/navigation';
 
-const MobileNavigation = () => {
+interface Props {
+  ownerId :string;
+  accountId:string;
+  fullName:string;
+  avatar:string;
+  email:string;
+}
+const MobileNavigation = ({ownerId, accountId, fullName, avatar, email} : Props) => {
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
   return (
-    <div>MobileNavigation</div>
+    <header>
+      <Image src="/assets/icons/logo-full-brand.svg" alt='logo' width={120} height={52} className='h-auto' />
+      <Sheet open={open} onOpenChange={setOpen} >
+        <SheetTrigger>
+          <Image src="/assets/icons/menu.svg" alt='Search' width={30} height={30}  />
+        </SheetTrigger>
+        <SheetContent className="shad-sheet h-screen px-3">
+            <SheetTitle>
+              <div className='headr-user'>
+                <Image src={avatar} alt='avatar' width={44} height={44} className='header-user-avatar' />
+                <div className="sm:hidden lg:block">
+                  <p className='subtitle-2 capitalize'> {fullName} </p>
+                  <p className="caption">{email}</p>
+                </div>
+              </div>  
+            </SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </SheetDescription>
+        </SheetContent>
+      </Sheet>
+    </header>
   )
 }
 
